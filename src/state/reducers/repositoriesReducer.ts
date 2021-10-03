b4590b6f9f775d653dbc1b5e25b8edc1a1bad12a
jace <bellcastle88@gmail.com>
2021-10-04 06:18:19 +0900

    Commit

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,20 +1,27 @@
-interface Repositoriestate {
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+interface RepositoriesState {
     loading: boolean;
     error: string | null;
     data: string[];
-}
-
-const reducer = (state: Repositoriestate, action : any) => {
+  }
+  
+  const reducer = (
+    state: RepositoriesState,
+    action: Action
+  ): RepositoriesState => {
     switch (action.type) {
-        case 'search_repositories':
-            return { loading: true, error: null, data:[]};
-        case 'search_repositories_success':
-            return { loading: false, error: null, data: action.payload }
-        case 'search_repositories_error':
-            return { loading: false, error: action.payload, data: [] }
-        default:
-            return state;
+      case ActionType.SEARCH_REPOSITORIES:
+        return { loading: true, error: null, data: [] };
+      case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+        return { loading: false, error: null, data: action.payload };
+      case ActionType.SEARCH_REPOSITORIES_ERROR:
+        return { loading: false, error: action.payload, data: [] };
+      default:
+        return state;
     }
-}
-
-export default reducer;
+  };
+  
+  export default reducer;
+  
